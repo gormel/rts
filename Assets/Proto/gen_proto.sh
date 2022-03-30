b4fc3a3ce181660302f67934449f7d3110324fc7
Roman <nope@nope.nope>
2022-03-30 14:10:08 +0300

    *linux proto generation fixed

--- Assets/Proto/gen_proto.sh
+++ Assets/Proto/gen_proto.sh
@@ -1,3 +1,3 @@
-rm -rf ./dst
+rm -rf ./dst/*
 mkdir dst
-ls src/*.proto | xargs -I {} protoc -Isrc {} --csharp_out=./dst --grpc_out=./dst --plugin=protoc-gen-grpc=/usr/bin/grpc_csharp_plugin
+ls src/*.proto | xargs -I {} wine ./tools/protoc.exe -Isrc {} --csharp_out=./dst --plugin=protoc-gen-grpc=./tools/grpc_csharp_plugin.exe --grpc_out=./dst
