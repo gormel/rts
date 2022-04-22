b71ac8dbb71baaa214250bbc172d08cc2b2cf7c2
Roman <nope@nope.nope>
2022-04-22 17:10:48 +0300

    *warrior strategy state is on orders panel

--- Assets/Interaction/SelectedActions/SelectedWarriorActionsInterface.cs
+++ Assets/Interaction/SelectedActions/SelectedWarriorActionsInterface.cs
@@ -1,11 +1,29 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Assets.Core.GameObjects.Base;
 using Assets.Views.Base;
+using UnityEngine;
 
 namespace Assets.Interaction
 {
     class SelectedWarriorActionsInterface : SelectedUnitsActionsInterface
     {
+        public GameObject IdleHighliter;
+        public GameObject AggressiveHighliter;
+        public GameObject DefenciveHighliter;
+
+        private void Update()
+        {
+            var warrior = Interface.FetchSelectedInfo<IWarriorInfo>().FirstOrDefault();
+            if (warrior != null)
+            {
+                var strategy = warrior.Strategy;
+                IdleHighliter.SetActive(strategy == Strategy.Idle);
+                AggressiveHighliter.SetActive(strategy == Strategy.Aggressive);
+                DefenciveHighliter.SetActive(strategy == Strategy.Defencive);
+            }
+        }
+
         public void BeginAttack()
         {
             Interface.BeginAttack(Interface.FetchSelectedOrders<IWarriorOrders>());
